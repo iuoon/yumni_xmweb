884c2a5b75bb3d5bd6f92b1bd7d00c61b707529f
iuoon <iuoon@163.com>
2018-08-01 12:47:55 +0800

    目录调整

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'Vuex'
 
-import UserStore from '../components/store/UserStore.js' // 引入userstore对象
-import OtherStore from '../components/store/OtherStore.js'
+import UserStore from '../store/UserStore.js' // 引入userstore对象
+import OtherStore from '../store/OtherStore.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
